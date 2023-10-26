263efe58578e17e0f5c34962b9c48dc0ef03f3d4
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-10-26 18:04:44 +0300

    auth first part

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -5,4 +5,9 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoZmlzZGppcmVueXBocXdid3dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc4NTQ4ODYsImV4cCI6MjAxMzQzMDg4Nn0.66YAb1PCHPiU8Qytl6uMbEdXR53cPFm0M7ZeMmW9xyw";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+/**
+ * jonas@example.com
+ * PASS0987
+ */
+
 export default supabase;
